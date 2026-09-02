786fb1655833847b154956d8e6196125e2c157ff
Ahanaf Mokammel <ahanafmo@gmail.com>
2026-09-02 15:39:49 +0600

    p

--- src/Web/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Platform.Web.Controllers;
-
-public class HomeController : Controller
-{
-    [Route("/Home/Error")]
-    public IActionResult Error() => View();
-}
--- src/Web/Models/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Platform.Web.Models;
-
-public class ApplicationUser : IdentityUser
-{
-    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
-}
